100acc3c6c5ee062c69200b9ff1ccc0552349929
Thomasperge <thomaskauffmant74@gmail.com>
2023-10-17 09:33:30 +0200

    feat: adding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AllProjectsComponent } from './all-projects/all-projects.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AllProjectsComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
